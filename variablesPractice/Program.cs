f2113890db8f637e6f55c5af326e5da7cc967939
Baines Deal <bainesdeal@gmail.com>
2022-05-31 11:09:36 -0500

    initial commit for variablesPractice

---
+++ variablesPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿
+            string name = "Baines";
+            int age = 27;
+            char dog = 'y';
+            bool maritalStatus = false;
+            double birthday = 4.18;
+            var salary = 6.38E4;
+
+            Console.WriteLine ("Hello. My name is {0}, and I am {1} years old.", name, age);
+            Console.WriteLine ("If you ask the question 'Do you have a dog?'");
+            Console.WriteLine ("My answer would be {0}", dog);
+            Console.WriteLine ("If you were trying to determine my marital status it would return {0}", maritalStatus);
+            Console.WriteLine ("My birthday is {0}. I am taking part in TrueCoders because \nI am trying to earn a salary making more than {1}.", birthday, salary); 
